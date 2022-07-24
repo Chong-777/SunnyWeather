f64844dc790c0f05839608151114e72abd8b6043
Tom <Tony@qq.com>
2022-07-24 11:56:31 +0800

    other feature

--- app/src/main/java/com/example/sunnyweather/ui/place/PlaceFragment.java
+++ app/src/main/java/com/example/sunnyweather/ui/place/PlaceFragment.java
@@ -3,6 +3,6 @@ package com.example.sunnyweather.ui.place;
 import androidx.fragment.app.Fragment;
 
 public class PlaceFragment extends Fragment {
-
+    private static final String TAG = "Cannot invoke method length() on null object";
 
 }
